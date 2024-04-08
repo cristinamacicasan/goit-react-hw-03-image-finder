adfb1179f056af0c93bd6bcb2b900905a3fa3bb2
cristinamacicasan <cmacicasan@gmail.com>
2024-04-08 07:53:52 +0300

    finish searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+
+import React from 'react';
+import Searchbar from './Searchbar';
 export const App = () => {
   return (
     <div
@@ -10,7 +13,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Searchbar/>
     </div>
   );
 };
